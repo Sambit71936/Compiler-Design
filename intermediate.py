360c8bf4a300bc56fe9735d66c749b5027c0f75f
Sambit71936 <156590298+Sambit71936@users.noreply.github.com>
2026-02-23 19:46:42 +0530

    Add files via upload

---
+++ intermediate.py
@@ -0,0 +1,16 @@
+def run_intermediate():
+    import re
+
+    print("\n--- PHASE 4: INTERMEDIATE CODE ---\n")
+
+    temp = 1
+    line = input("Enter the expression: ")
+
+    line = line.strip()
+    if "+" in line:
+        lhs, rhs = line.split("=")
+        a, b = rhs.split("+")
+        print(f"t{temp} = {a.strip()} + {b.strip()}")
+        print(f"{lhs.strip()} = t{temp}")
+    elif "=" in line:
+        print(line)
