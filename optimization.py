7927b969c1d10a449bd4caf5fe5fb933c90bcce5
Sambit71936 <156590298+Sambit71936@users.noreply.github.com>
2026-02-23 19:54:48 +0530

    Delete optimization.py

--- optimization.py
+++
@@ -1,8 +0,0 @@
-def run_optimization():
-    print("\n--- PHASE 5: CODE OPTIMIZATION ---\n")
-
-    with open("input.txt", "r") as file:
-        lines = file.readlines()
-
-    for line in lines:
-        print(line.strip())
